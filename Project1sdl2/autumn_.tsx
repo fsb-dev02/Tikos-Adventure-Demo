6a7d11d143e82f5d599004960847e3c15dd6bcd7
fsb-dev02 <filipesantos_barbosa@outlook.com>
2026-07-12 04:41:47 -0300

    Remove obsolete files

--- Project1sdl2/autumn_.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="autumn_" tilewidth="16" tileheight="16" tilecount="220" columns="11">
- <image source="../../../../Downloads/opengameart/itch/fourSeasonsPlatformer_ [tileset, version 2.0]/fourSeasonsPlatformer_ [tileset, version 2.0]/midground_/autumn_.png" width="176" height="320"/>
-</tileset>
